4394fd323759f9bf006f374780a9534831fad1e4
Poveii <yellowtutors@gmail.com>
2024-01-17 17:08:56 -0300

    chore: SQLAlchemy sqlite config

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-# Importação do Flask
+# Importações
 ## É ao contrário do JavaScript kkkkkkk
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
 
 # Definição de uma rota raiz (página inicial) e da função que será executada ao request.
 @app.route('/teste')
