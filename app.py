ad7ecce358e2068991d1df5fae006465d04388f9
Poveii <yellowtutors@gmail.com>
2024-01-17 17:34:04 -0300

    feat: Product table

--- app.py
+++ app.py
@@ -8,6 +8,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
 
+# Modelagem
+# Produto (id, name, price, description)
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
 # Definição de uma rota raiz (página inicial) e da função que será executada ao request.
 @app.route('/teste')
 def hello_world():
