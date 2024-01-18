4e85f1d94fa1a227ea531896651c71b95d79df61
Poveii <yellowtutors@gmail.com>
2024-01-18 18:10:01 -0300

    feat: CartItem table and User cart relation

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=True)
+    cart = db.relationship('CartItem', backref='user', lazy=True)
 
 # Produto (id, name, price, description)
 class Product(db.Model):
@@ -29,6 +30,11 @@ class Product(db.Model):
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
+
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 
 # Autenticação
 @login_manager.user_loader
