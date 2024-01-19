9cfa47bea152bb1b4a021cceb8cb4295df36e769
Poveii <yellowtutors@gmail.com>
2024-01-19 15:17:33 -0300

    feat: `remove_from_cart` route

--- app.py
+++ app.py
@@ -150,6 +150,19 @@ def add_to_cart(product_id):
 
     return jsonify({ "message": "Failed to add item to the cart" }), 400
 
+@app.route('/api/cart/remove/<int:product_id>', methods=['DELETE'])
+@login_required
+def remove_from_cart(product_id):
+    # Produto, Usuário = Item do Carrinho
+    cart_item = CartItem.query.filter_by(user_id=current_user.id, product_id=product_id).first()
+
+    if cart_item:
+        db.session.delete(cart_item)
+        db.session.commit()
+        return jsonify({ "message": "Item removed from the cart successfully" })
+
+    return jsonify({ "message": "Failed to remove item from the cart" }), 400
+
 # Definição de uma rota raiz (página inicial) e da função que será executada ao request.
 @app.route('/teste')
 def hello_world():
